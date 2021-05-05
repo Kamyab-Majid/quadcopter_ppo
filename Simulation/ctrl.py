30e5766eeeb308ad50649a507b28dcacd27af95f
Adrian <kamyab@ualberta.ca>
2021-05-05 17:17:04 -0600

    initial changes

--- Simulation/ctrl.py
+++ Simulation/ctrl.py
@@ -19,16 +19,16 @@ from numpy.linalg import norm
 import utils
 import config
 
-rad2deg = 180.0/pi
-deg2rad = pi/180.0
+rad2deg = 180.0 / pi
+deg2rad = pi / 180.0
 
 # Set PID Gains and Max Values
 # ---------------------------
 
 # Position P gains
-Py    = 1.0
-Px    = Py
-Pz    = 2.0
+Py = 1.0
+Px = Py
+Pz = 2.0
 
 pos_P_gain = np.array([Px, Py, Pz])
 
@@ -62,7 +62,7 @@ Pp = 1.5
 Dp = 0.04
 
 Pq = Pp
-Dq = Dp 
+Dq = Dp
 
 Pr = 1.0
 Dr = 0.1
@@ -81,56 +81,54 @@ velMaxAll = 5.0
 saturateVel_separetely = False
 
 # Max tilt
-tiltMax = 50.0*deg2rad
+tiltMax = 50.0 * deg2rad
 
 # Max Rate
-pMax = 200.0*deg2rad
-qMax = 200.0*deg2rad
-rMax = 150.0*deg2rad
+pMax = 200.0 * deg2rad
+qMax = 200.0 * deg2rad
+rMax = 150.0 * deg2rad
 
 rateMax = np.array([pMax, qMax, rMax])
 
 
 class Control:
-    
     def __init__(self, quad, yawType):
         self.sDesCalc = np.zeros(16)
-        self.w_cmd = np.ones(4)*quad.params["w_hover"]
+        self.w_cmd = np.ones(4) * quad.params["w_hover"]
         self.thr_int = np.zeros(3)
-        if (yawType == 0):
+        if yawType == 0:
             att_P_gain[2] = 0
         self.setYawWeight()
-        self.pos_sp    = np.zeros(3)
-        self.vel_sp    = np.zeros(3)
-        self.acc_sp    = np.zeros(3)
+        self.pos_sp = np.zeros(3)
+        self.vel_sp = np.zeros(3)
+        self.acc_sp = np.zeros(3)
         self.thrust_sp = np.zeros(3)
-        self.eul_sp    = np.zeros(3)
-        self.pqr_sp    = np.zeros(3)
-        self.yawFF     = np.zeros(3)
+        self.eul_sp = np.zeros(3)
+        self.pqr_sp = np.zeros(3)
+        self.yawFF = np.zeros(3)
 
-    
     def controller(self, traj, quad, sDes, Ts):
 
         # Desired State (Create a copy, hence the [:])
         # ---------------------------
-        self.pos_sp[:]    = traj.sDes[0:3]
-        self.vel_sp[:]    = traj.sDes[3:6]
-        self.acc_sp[:]    = traj.sDes[6:9]
+        self.pos_sp[:] = traj.sDes[0:3]
+        self.vel_sp[:] = traj.sDes[3:6]
+        self.acc_sp[:] = traj.sDes[6:9]
         self.thrust_sp[:] = traj.sDes[9:12]
-        self.eul_sp[:]    = traj.sDes[12:15]
-        self.pqr_sp[:]    = traj.sDes[15:18]
-        self.yawFF[:]     = traj.sDes[18]
-        
+        self.eul_sp[:] = traj.sDes[12:15]
+        self.pqr_sp[:] = traj.sDes[15:18]
+        self.yawFF[:] = traj.sDes[18]
+
         # Select Controller
         # ---------------------------
-        if (traj.ctrlType == "xyz_vel"):
+        if traj.ctrlType == "xyz_vel":
             self.saturateVel()
             self.z_vel_control(quad, Ts)
             self.xy_vel_control(quad, Ts)
             self.thrustToAttitude(quad, Ts)
             self.attitude_control(quad, Ts)
             self.rate_control(quad, Ts)
-        elif (traj.ctrlType == "xy_vel_z_pos"):
+        elif traj.ctrlType == "xy_vel_z_pos":
             self.z_pos_control(quad, Ts)
             self.saturateVel()
             self.z_vel_control(quad, Ts)
@@ -138,7 +136,7 @@ class Control:
             self.thrustToAttitude(quad, Ts)
             self.attitude_control(quad, Ts)
             self.rate_control(quad, Ts)
-        elif (traj.ctrlType == "xyz_pos"):
+        elif traj.ctrlType == "xyz_pos":
             self.z_pos_control(quad, Ts)
             self.xy_pos_control(quad, Ts)
             self.saturateVel()
@@ -149,65 +147,71 @@ class Control:
             self.rate_control(quad, Ts)
 
         # Mixer
-        # --------------------------- 
+        # ---------------------------
         self.w_cmd = utils.mixerFM(quad, norm(self.thrust_sp), self.rateCtrl)
-        
+
         # Add calculated Desired States
-        # ---------------------------         
+        # ---------------------------
         self.sDesCalc[0:3] = self.pos_sp
         self.sDesCalc[3:6] = self.vel_sp
         self.sDesCalc[6:9] = self.thrust_sp
         self.sDesCalc[9:13] = self.qd
         self.sDesCalc[13:16] = self.rate_sp
 
-
     def z_pos_control(self, quad, Ts):
-       
+
         # Z Position Control
-        # --------------------------- 
+        # ---------------------------
         pos_z_error = self.pos_sp[2] - quad.pos[2]
-        self.vel_sp[2] += pos_P_gain[2]*pos_z_error
-        
-    
+        self.vel_sp[2] += pos_P_gain[2] * pos_z_error
+
     def xy_pos_control(self, quad, Ts):
 
         # XY Position Control
-        # --------------------------- 
-        pos_xy_error = (self.pos_sp[0:2] - quad.pos[0:2])
-        self.vel_sp[0:2] += pos_P_gain[0:2]*pos_xy_error
-        
-        
+        # ---------------------------
+        pos_xy_error = self.pos_sp[0:2] - quad.pos[0:2]
+        self.vel_sp[0:2] += pos_P_gain[0:2] * pos_xy_error
+
     def saturateVel(self):
 
         # Saturate Velocity Setpoint
-        # --------------------------- 
+        # ---------------------------
         # Either saturate each velocity axis separately, or total velocity (prefered)
-        if (saturateVel_separetely):
+        if saturateVel_separetely:
             self.vel_sp = np.clip(self.vel_sp, -velMax, velMax)
         else:
             totalVel_sp = norm(self.vel_sp)
-            if (totalVel_sp > velMaxAll):
-                self.vel_sp = self.vel_sp/totalVel_sp*velMaxAll
-
+            if totalVel_sp > velMaxAll:
+                self.vel_sp = self.vel_sp / totalVel_sp * velMaxAll
 
     def z_vel_control(self, quad, Ts):
-        
+
         # Z Velocity Control (Thrust in D-direction)
         # ---------------------------
-        # Hover thrust (m*g) is sent as a Feed-Forward term, in order to 
+        # Hover thrust (m*g) is sent as a Feed-Forward term, in order to
         # allow hover when the position and velocity error are nul
         vel_z_error = self.vel_sp[2] - quad.vel[2]
-        if (config.orient == "NED"):
-            thrust_z_sp = vel_P_gain[2]*vel_z_error - vel_D_gain[2]*quad.vel_dot[2] + quad.params["mB"]*(self.acc_sp[2] - quad.params["g"]) + self.thr_int[2]
-        elif (config.orient == "ENU"):
-            thrust_z_sp = vel_P_gain[2]*vel_z_error - vel_D_gain[2]*quad.vel_dot[2] + quad.params["mB"]*(self.acc_sp[2] + quad.params["g"]) + self.thr_int[2]
-        
+        if config.orient == "NED":
+            thrust_z_sp = (
+                vel_P_gain[2] * vel_z_error
+                - vel_D_gain[2] * quad.vel_dot[2]
+                + quad.params["mB"] * (self.acc_sp[2] - quad.params["g"])
+                + self.thr_int[2]
+            )
+        elif config.orient == "ENU":
+            thrust_z_sp = (
+                vel_P_gain[2] * vel_z_error
+                - vel_D_gain[2] * quad.vel_dot[2]
+                + quad.params["mB"] * (self.acc_sp[2] + quad.params["g"])
+                + self.thr_int[2]
+            )
+
         # Get thrust limits
-        if (config.orient == "NED"):
+        if config.orient == "NED":
             # The Thrust limits are negated and swapped due to NED-frame
             uMax = -quad.params["minThr"]
             uMin = -quad.params["maxThr"]
-        elif (config.orient == "ENU"):
+        elif config.orient == "ENU":
             uMax = quad.params["maxThr"]
             uMin = quad.params["minThr"]
 
@@ -216,56 +220,60 @@ class Control:
 
         # Calculate integral part
         if not (stop_int_D):
-            self.thr_int[2] += vel_I_gain[2]*vel_z_error*Ts * quad.params["useIntergral"]
+            self.thr_int[2] += vel_I_gain[2] * vel_z_error * Ts * quad.params["useIntergral"]
             # Limit thrust integral
-            self.thr_int[2] = min(abs(self.thr_int[2]), quad.params["maxThr"])*np.sign(self.thr_int[2])
+            self.thr_int[2] = min(abs(self.thr_int[2]), quad.params["maxThr"]) * np.sign(self.thr_int[2])
 
         # Saturate thrust setpoint in D-direction
         self.thrust_sp[2] = np.clip(thrust_z_sp, uMin, uMax)
 
-    
     def xy_vel_control(self, quad, Ts):
-        
+
         # XY Velocity Control (Thrust in NE-direction)
         # ---------------------------
         vel_xy_error = self.vel_sp[0:2] - quad.vel[0:2]
-        thrust_xy_sp = vel_P_gain[0:2]*vel_xy_error - vel_D_gain[0:2]*quad.vel_dot[0:2] + quad.params["mB"]*(self.acc_sp[0:2]) + self.thr_int[0:2]
+        thrust_xy_sp = (
+            vel_P_gain[0:2] * vel_xy_error
+            - vel_D_gain[0:2] * quad.vel_dot[0:2]
+            + quad.params["mB"] * (self.acc_sp[0:2])
+            + self.thr_int[0:2]
+        )
 
         # Max allowed thrust in NE based on tilt and excess thrust
-        thrust_max_xy_tilt = abs(self.thrust_sp[2])*np.tan(tiltMax)
-        thrust_max_xy = sqrt(quad.params["maxThr"]**2 - self.thrust_sp[2]**2)
+        thrust_max_xy_tilt = abs(self.thrust_sp[2]) * np.tan(tiltMax)
+        thrust_max_xy = sqrt(quad.params["maxThr"] ** 2 - self.thrust_sp[2] ** 2)
         thrust_max_xy = min(thrust_max_xy, thrust_max_xy_tilt)
 
         # Saturate thrust in NE-direction
         self.thrust_sp[0:2] = thrust_xy_sp
-        if (np.dot(self.thrust_sp[0:2].T, self.thrust_sp[0:2]) > thrust_max_xy**2):
+        if np.dot(self.thrust_sp[0:2].T, self.thrust_sp[0:2]) > thrust_max_xy ** 2:
             mag = norm(self.thrust_sp[0:2])
-            self.thrust_sp[0:2] = thrust_xy_sp/mag*thrust_max_xy
-        
+            self.thrust_sp[0:2] = thrust_xy_sp / mag * thrust_max_xy
+
         # Use tracking Anti-Windup for NE-direction: during saturation, the integrator is used to unsaturate the output
         # see Anti-Reset Windup for PID controllers, L.Rundqwist, 1990
-        arw_gain = 2.0/vel_P_gain[0:2]
-        vel_err_lim = vel_xy_error - (thrust_xy_sp - self.thrust_sp[0:2])*arw_gain
-        self.thr_int[0:2] += vel_I_gain[0:2]*vel_err_lim*Ts * quad.params["useIntergral"]
-    
+        arw_gain = 2.0 / vel_P_gain[0:2]
+        vel_err_lim = vel_xy_error - (thrust_xy_sp - self.thrust_sp[0:2]) * arw_gain
+        self.thr_int[0:2] += vel_I_gain[0:2] * vel_err_lim * Ts * quad.params["useIntergral"]
+
     def thrustToAttitude(self, quad, Ts):
-        
+
         # Create Full Desired Quaternion Based on Thrust Setpoint and Desired Yaw Angle
         # ---------------------------
         yaw_sp = self.eul_sp[2]
 
         # Desired body_z axis direction
         body_z = -utils.vectNormalize(self.thrust_sp)
-        if (config.orient == "ENU"):
+        if config.orient == "ENU":
             body_z = -body_z
-        
+
         # Vector of desired Yaw direction in XY plane, rotated by pi/2 (fake body_y axis)
         y_C = np.array([-sin(yaw_sp), cos(yaw_sp), 0.0])
-        
+
         # Desired body_x axis direction
         body_x = np.cross(y_C, body_z)
         body_x = utils.vectNormalize(body_x)
-        
+
         # Desired body_y axis direction
         body_y = np.cross(body_z, body_x)
 
@@ -274,62 +282,61 @@ class Control:
 
         # Full desired quaternion (full because it considers the desired Yaw angle)
         self.qd_full = utils.RotToQuat(R_sp)
-        
-        
+
     def attitude_control(self, quad, Ts):
 
         # Current thrust orientation e_z and desired thrust orientation e_z_d
-        e_z = quad.dcm[:,2]
+        e_z = quad.dcm[:, 2]
         e_z_d = -utils.vectNormalize(self.thrust_sp)
-        if (config.orient == "ENU"):
+        if config.orient == "ENU":
             e_z_d = -e_z_d
 
         # Quaternion error between the 2 vectors
         qe_red = np.zeros(4)
-        qe_red[0] = np.dot(e_z, e_z_d) + sqrt(norm(e_z)**2 * norm(e_z_d)**2)
+        qe_red[0] = np.dot(e_z, e_z_d) + sqrt(norm(e_z) ** 2 * norm(e_z_d) ** 2)
         qe_red[1:4] = np.cross(e_z, e_z_d)
         qe_red = utils.vectNormalize(qe_red)
-        
+
         # Reduced desired quaternion (reduced because it doesn't consider the desired Yaw angle)
         self.qd_red = utils.quatMultiply(qe_red, quad.quat)
 
         # Mixed desired quaternion (between reduced and full) and resulting desired quaternion qd
         q_mix = utils.quatMultiply(utils.inverse(self.qd_red), self.qd_full)
-        q_mix = q_mix*np.sign(q_mix[0])
+        q_mix = q_mix * np.sign(q_mix[0])
         q_mix[0] = np.clip(q_mix[0], -1.0, 1.0)
         q_mix[3] = np.clip(q_mix[3], -1.0, 1.0)
-        self.qd = utils.quatMultiply(self.qd_red, np.array([cos(self.yaw_w*np.arccos(q_mix[0])), 0, 0, sin(self.yaw_w*np.arcsin(q_mix[3]))]))
+        self.qd = utils.quatMultiply(
+            self.qd_red, np.array([cos(self.yaw_w * np.arccos(q_mix[0])), 0, 0, sin(self.yaw_w * np.arcsin(q_mix[3]))])
+        )
 
         # Resulting error quaternion
         self.qe = utils.quatMultiply(utils.inverse(quad.quat), self.qd)
 
         # Create rate setpoint from quaternion error
-        self.rate_sp = (2.0*np.sign(self.qe[0])*self.qe[1:4])*att_P_gain
-        
+        self.rate_sp = (2.0 * np.sign(self.qe[0]) * self.qe[1:4]) * att_P_gain
+
         # Limit yawFF
         self.yawFF = np.clip(self.yawFF, -rateMax[2], rateMax[2])
 
         # Add Yaw rate feed-forward
-        self.rate_sp += utils.quat2Dcm(utils.inverse(quad.quat))[:,2]*self.yawFF
+        self.rate_sp += utils.quat2Dcm(utils.inverse(quad.quat))[:, 2] * self.yawFF
 
         # Limit rate setpoint
         self.rate_sp = np.clip(self.rate_sp, -rateMax, rateMax)
 
-
     def rate_control(self, quad, Ts):
-        
+
         # Rate Control
         # ---------------------------
         rate_error = self.rate_sp - quad.omega
-        self.rateCtrl = rate_P_gain*rate_error - rate_D_gain*quad.omega_dot     # Be sure it is right sign for the D part
-        
+        self.rateCtrl = (
+            rate_P_gain * rate_error - rate_D_gain * quad.omega_dot
+        )  # Be sure it is right sign for the D part
 
     def setYawWeight(self):
-        
+
         # Calculate weight of the Yaw control gain
-        roll_pitch_gain = 0.5*(att_P_gain[0] + att_P_gain[1])
-        self.yaw_w = np.clip(att_P_gain[2]/roll_pitch_gain, 0.0, 1.0)
+        roll_pitch_gain = 0.5 * (att_P_gain[0] + att_P_gain[1])
+        self.yaw_w = np.clip(att_P_gain[2] / roll_pitch_gain, 0.0, 1.0)
 
         att_P_gain[2] = roll_pitch_gain
-
-    
--- Simulation/run_3D_simulation.py
+++ Simulation/run_3D_simulation.py
@@ -18,51 +18,52 @@ from utils.windModel import Wind
 import utils
 import config
 
+
 def quad_sim(t, Ts, quad, ctrl, wind, traj):
-    
+
     # Dynamics (using last timestep's commands)
     # ---------------------------
     quad.update(t, Ts, ctrl.w_cmd, wind)
     t += Ts
 
-    # Trajectory for Desired States 
+    # Trajectory for Desired States
     # ---------------------------
-    sDes = traj.desiredState(t, Ts, quad)        
+    sDes = traj.desiredState(t, Ts, quad)
 
     # Generate Commands (for next iteration)
     # ---------------------------
     ctrl.controller(traj, quad, sDes, Ts)
 
     return t
-    
+
 
 def main():
     start_time = time.time()
 
     # Simulation Setup
-    # --------------------------- 
+    # ---------------------------
     Ti = 0
     Ts = 0.005
     Tf = 18
     ifsave = 0
 
     # Choose trajectory settings
-    # --------------------------- 
+    # ---------------------------
     ctrlOptions = ["xyz_pos", "xy_vel_z_pos", "xyz_vel"]
     trajSelect = np.zeros(3)
 
     # Select Control Type             (0: xyz_pos,                  1: xy_vel_z_pos,            2: xyz_vel)
-    ctrlType = ctrlOptions[0]   
-    # Select Position Trajectory Type (0: hover,                    1: pos_waypoint_timed,      2: pos_waypoint_interp,    
+    ctrlType = ctrlOptions[0]
+    # Select Position Trajectory Type (0: hover,                    1: pos_waypoint_timed,      2: pos_waypoint_interp,
     #                                  3: minimum velocity          4: minimum accel,           5: minimum jerk,           6: minimum snap
     #                                  7: minimum accel_stop        8: minimum jerk_stop        9: minimum snap_stop
     #                                 10: minimum jerk_full_stop   11: minimum snap_full_stop
     #                                 12: pos_waypoint_arrived
-    trajSelect[0] = 6         
+    trajSelect[0] = 0  # (changed): was 6
     # Select Yaw Trajectory Type      (0: none                      1: yaw_waypoint_timed,      2: yaw_waypoint_interp     3: follow          4: zero)
-    trajSelect[1] = 3           
+    trajSelect[1] = 0  # (changed): was 6
     # Select if waypoint time is used, or if average speed is used to calculate waypoint time   (0: waypoint time,   1: average speed)
-    trajSelect[2] = 0           
+    trajSelect[2] = 0
     print("Control type: {}".format(ctrlType))
 
     # Initialize Quadcopter, Controller, Wind, Result Matrixes
@@ -70,73 +71,73 @@ def main():
     quad = Quadcopter(Ti)
     traj = Trajectory(quad, ctrlType, trajSelect)
     ctrl = Control(quad, traj.yawType)
-    wind = Wind('None', 2.0, 90, -15)
+    wind = Wind("None", 2.0, 90, -15)
 
     # Trajectory for First Desired States
     # ---------------------------
-    sDes = traj.desiredState(0, Ts, quad)        
+    sDes = traj.desiredState(0, Ts, quad)
 
     # Generate First Commands
     # ---------------------------
     ctrl.controller(traj, quad, sDes, Ts)
-    
+
     # Initialize Result Matrixes
     # ---------------------------
-    numTimeStep = int(Tf/Ts+1)
+    numTimeStep = int(Tf / Ts + 1)
 
-    t_all          = np.zeros(numTimeStep)
-    s_all          = np.zeros([numTimeStep, len(quad.state)])
-    pos_all        = np.zeros([numTimeStep, len(quad.pos)])
-    vel_all        = np.zeros([numTimeStep, len(quad.vel)])
-    quat_all       = np.zeros([numTimeStep, len(quad.quat)])
-    omega_all      = np.zeros([numTimeStep, len(quad.omega)])
-    euler_all      = np.zeros([numTimeStep, len(quad.euler)])
-    sDes_traj_all  = np.zeros([numTimeStep, len(traj.sDes)])
-    sDes_calc_all  = np.zeros([numTimeStep, len(ctrl.sDesCalc)])
-    w_cmd_all      = np.zeros([numTimeStep, len(ctrl.w_cmd)])
-    wMotor_all     = np.zeros([numTimeStep, len(quad.wMotor)])
-    thr_all        = np.zeros([numTimeStep, len(quad.thr)])
-    tor_all        = np.zeros([numTimeStep, len(quad.tor)])
+    t_all = np.zeros(numTimeStep)
+    s_all = np.zeros([numTimeStep, len(quad.state)])
+    pos_all = np.zeros([numTimeStep, len(quad.pos)])
+    vel_all = np.zeros([numTimeStep, len(quad.vel)])
+    quat_all = np.zeros([numTimeStep, len(quad.quat)])
+    omega_all = np.zeros([numTimeStep, len(quad.omega)])
+    euler_all = np.zeros([numTimeStep, len(quad.euler)])
+    sDes_traj_all = np.zeros([numTimeStep, len(traj.sDes)])
+    sDes_calc_all = np.zeros([numTimeStep, len(ctrl.sDesCalc)])
+    w_cmd_all = np.zeros([numTimeStep, len(ctrl.w_cmd)])
+    wMotor_all = np.zeros([numTimeStep, len(quad.wMotor)])
+    thr_all = np.zeros([numTimeStep, len(quad.thr)])
+    tor_all = np.zeros([numTimeStep, len(quad.tor)])
 
-    t_all[0]            = Ti
-    s_all[0,:]          = quad.state
-    pos_all[0,:]        = quad.pos
-    vel_all[0,:]        = quad.vel
-    quat_all[0,:]       = quad.quat
-    omega_all[0,:]      = quad.omega
-    euler_all[0,:]      = quad.euler
-    sDes_traj_all[0,:]  = traj.sDes
-    sDes_calc_all[0,:]  = ctrl.sDesCalc
-    w_cmd_all[0,:]      = ctrl.w_cmd
-    wMotor_all[0,:]     = quad.wMotor
-    thr_all[0,:]        = quad.thr
-    tor_all[0,:]        = quad.tor
+    t_all[0] = Ti
+    s_all[0, :] = quad.state
+    pos_all[0, :] = quad.pos
+    vel_all[0, :] = quad.vel
+    quat_all[0, :] = quad.quat
+    omega_all[0, :] = quad.omega
+    euler_all[0, :] = quad.euler
+    sDes_traj_all[0, :] = traj.sDes
+    sDes_calc_all[0, :] = ctrl.sDesCalc
+    w_cmd_all[0, :] = ctrl.w_cmd
+    wMotor_all[0, :] = quad.wMotor
+    thr_all[0, :] = quad.thr
+    tor_all[0, :] = quad.tor
 
     # Run Simulation
     # ---------------------------
     t = Ti
     i = 1
-    while round(t,3) < Tf:
-        
+    while round(t, 3) < Tf:
+
         t = quad_sim(t, Ts, quad, ctrl, wind, traj)
-        
+
         # print("{:.3f}".format(t))
-        t_all[i]             = t
-        s_all[i,:]           = quad.state
-        pos_all[i,:]         = quad.pos
-        vel_all[i,:]         = quad.vel
-        quat_all[i,:]        = quad.quat
-        omega_all[i,:]       = quad.omega
-        euler_all[i,:]       = quad.euler
-        sDes_traj_all[i,:]   = traj.sDes
-        sDes_calc_all[i,:]   = ctrl.sDesCalc
-        w_cmd_all[i,:]       = ctrl.w_cmd
-        wMotor_all[i,:]      = quad.wMotor
-        thr_all[i,:]         = quad.thr
-        tor_all[i,:]         = quad.tor
-        
+        t_all[i] = t
+        s_all[i, :] = quad.state
+        pos_all[i, :] = quad.pos
+        vel_all[i, :] = quad.vel
+        quat_all[i, :] = quad.quat
+        omega_all[i, :] = quad.omega
+        euler_all[i, :] = quad.euler
+        sDes_traj_all[i, :] = traj.sDes
+        sDes_calc_all[i, :] = ctrl.sDesCalc
+        w_cmd_all[i, :] = ctrl.w_cmd
+        wMotor_all[i, :] = quad.wMotor
+        thr_all[i, :] = quad.thr
+        tor_all[i, :] = quad.tor
+
         i += 1
-    
+
     end_time = time.time()
     print("Simulated {:.2f}s in {:.6f}s.".format(t, end_time - start_time))
 
@@ -144,12 +145,29 @@ def main():
     # ---------------------------
 
     # utils.fullprint(sDes_traj_all[:,3:6])
-    utils.makeFigures(quad.params, t_all, pos_all, vel_all, quat_all, omega_all, euler_all, w_cmd_all, wMotor_all, thr_all, tor_all, sDes_traj_all, sDes_calc_all)
-    ani = utils.sameAxisAnimation(t_all, traj.wps, pos_all, quat_all, sDes_traj_all, Ts, quad.params, traj.xyzType, traj.yawType, ifsave)
+    utils.makeFigures(
+        quad.params,
+        t_all,
+        pos_all,
+        vel_all,
+        quat_all,
+        omega_all,
+        euler_all,
+        w_cmd_all,
+        wMotor_all,
+        thr_all,
+        tor_all,
+        sDes_traj_all,
+        sDes_calc_all,
+    )
+    ani = utils.sameAxisAnimation(
+        t_all, traj.wps, pos_all, quat_all, sDes_traj_all, Ts, quad.params, traj.xyzType, traj.yawType, ifsave
+    )
     plt.show()
 
+
 if __name__ == "__main__":
-    if (config.orient == "NED" or config.orient == "ENU"):
+    if config.orient == "NED" or config.orient == "ENU":
         main()
         # cProfile.run('main()')
     else:
